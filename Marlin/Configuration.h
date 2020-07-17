cc2bea52e0f27a4c57c1d14aaa07ac521c75ef69
SuperBest27 <64040600+SuperBest27@users.noreply.github.com>
2020-07-17 17:27:35 +0200

    BMG extruder Esteps modification

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -747,7 +747,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 320, 320, 1600, 1000 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 320, 320, 1600, 1670 }
 
 /**
  * Default Max Feed Rate (mm/s)
