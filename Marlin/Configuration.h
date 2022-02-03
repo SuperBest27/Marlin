768c2c024125a626d0d3eaff88c528e988b42433
SuperBest27 <64040600+SuperBest27@users.noreply.github.com>
2022-02-03 21:43:43 +0100

    PID Changes

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -603,9 +603,9 @@
     #define DEFAULT_Ki_LIST {   1.54,   1.54 }
     #define DEFAULT_Kd_LIST {  76.55,  76.55 }
   #else
-    #define DEFAULT_Kp  21.73
-    #define DEFAULT_Ki   1.54
-    #define DEFAULT_Kd  76.55
+    #define DEFAULT_Kp 20.46
+    #define DEFAULT_Ki 1.72
+    #define DEFAULT_Kd 60.97
   #endif
 #endif // PIDTEMP
 
@@ -642,9 +642,9 @@
   //#define MIN_BED_POWER 0
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
 
-  #define DEFAULT_bedKp 50.71
-  #define DEFAULT_bedKi 9.88
-  #define DEFAULT_bedKd 173.43
+  #define DEFAULT_bedKp 84.36
+  #define DEFAULT_bedKi 16.48
+  #define DEFAULT_bedKd 287.95
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
